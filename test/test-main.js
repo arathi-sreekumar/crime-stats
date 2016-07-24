a5d58857dec110363921ac85291c115182c78d2d
Arathi Sreekumar <arathi@Arathis-MacBook-Air.local>
2016-07-24 10:26:42 +0100

    Committing test config

---
+++ test/test-main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
+
+var pathToModule = function(path) {
+  var returnValue = path.replace(/^\/base\//, '').replace(/\.js$/, '');
+  return returnValue;
+};
+
+Object.keys(window.__karma__.files).forEach(function(file) {
+  if (TEST_REGEXP.test(file)) {
+    allTestFiles.push(pathToModule(file));
+  }
+});
+
+require.config({
+  baseUrl: '/base/application',
+  urlArgs: 'cb=' + Math.random(),
+  paths: {
+    text: '../js/libs/text',
+    jquery: '../js/libs/jquery-3.0.0.min',
+    underscore: '../js/libs/underscore.min',
+    backbone: '../js/libs/backbone.min',
+    sinon: '../js/libs/sinon',
+    handlebars: '../js/libs/handlebars.min',
+    router: '../js/application/router',
+    templates: '../templates',
+    views: '../js/application/views',
+    models: '../js/application/models',
+    collections: '../js/application/collections',
+    test: '../test'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery', 'handlebars'],
+      exports: 'Backbone'
+    }
+  },
+  deps: allTestFiles,
+  callback: window.__karma__.start
+});
+
